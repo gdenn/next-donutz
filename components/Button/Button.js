28cfab44174ba18347951c97e1c7f28da1ba1519
Dennis Groß <gdennis91@googlemail.com>
2021-08-21 21:19:29 +0200

    Improve button border

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -2,8 +2,8 @@ import classNames from "classnames"
 
 
 const BASE_CLASSES = "flex flex-row place-items-center rounded py-2 px-4 font-bold "
-const SECONDARY_CLASSES = "border hover:border-transparent hover:bg-white hover:text-blue-500 bg-transparent text-white border-white"
-const PRIMARY_CLASSES = "border bg-blue-500 hover:bg-blue-700 text-white"
+const SECONDARY_CLASSES = "border-2 hover:border-transparent hover:bg-white hover:text-blue-500 bg-transparent text-white border-white"
+const PRIMARY_CLASSES = "bg-blue-500 hover:bg-blue-700 text-white"
 
 const Button = ({
     children, 
